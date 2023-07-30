ba218836264ada04089552c71f50c0dd0588695f
Kiane L. Casagrande <kiane.casagrande@gmail.com>
2023-07-30 14:49:06 -0300

    style: Melhorar identação do código para distribuição de clientes
    - Corrigir espaçamentos para seguir padrões de formatação do projeto.
    
    As alterações melhoram a legibilidade e organização do código para a distribuição equilibrada de clientes entre os CSs disponíveis.

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const customerSuccess = [
     { id: 1, score: 60 },
     { id: 2, score: 70 },
     { id: 3, score: 90 },
-  ];
-  
-  const customers = [
+];
+
+const customers = [
     { id: 3, score: 80 },
     { id: 4, score: 90 },
     { id: 5, score: 50 },
     { id: 6, score: 70 },
-  ];
-  
-  const csAway = [2];
-  
-  const result = customerSuccessBalancing(customerSuccess, customers, csAway);
-  
-  console.log('Resultado do customerSuccessBalancing:', result);
+];
+
+const csAway = [2];
+
+const result = customerSuccessBalancing(customerSuccess, customers, csAway);
+
+console.log('Resultado do customerSuccessBalancing:', result);
