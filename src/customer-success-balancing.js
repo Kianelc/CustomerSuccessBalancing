8ee367af213bc63d49e1599d20ed5f7b0f4da625
Kiane L. Casagrande <kiane.casagrande@gmail.com>
2023-07-30 14:48:28 -0300

    feat: Adicionar funções para distribuição de clientes
    - Adicionar função setCustomersServedForCSs para inicializar a lista de clientes atendidos por cada CS.
    - Adicionar função sortByAscendingOrder para ordenar os CSs e clientes em ordem crescente com base no score.
    - Adicionar função distributeCustomersToCSs para distribuir clientes entre os CSs disponíveis.
    - Adicionar função filterAvailableCustomerSuccess para filtrar os CSs disponíveis.
    - Adicionar função findAvailableCSWithCapacity para encontrar CSs disponíveis com capacidade para atender o cliente.
    
    As funções permitem a distribuição equilibrada dos clientes entre os CSs disponíveis, garantindo que os clientes sejam atendidos por CSs com a capacidade apropriada.

--- src/customer-success-balancing.js
+++ src/customer-success-balancing.js
@@ -9,16 +9,50 @@ function customerSuccessBalancing(
   customers,
   customerSuccessAway
 ) {
-  console.log('customers ', customers);
-  console.log('customerSuccess ', customerSuccess);
-  console.log('customerSuccessAway ', customerSuccessAway);
-  /**
-   * ===============================================
-   * =========== Write your solution here ==========
-   * ===============================================
-   */
+  const updatedCustomerSuccess = setCustomersServedForCSs(customerSuccess);
+
+  const employeesSorted = sortByAscendingOrder(updatedCustomerSuccess, "score");
+  console.log('employeesSorted ', employeesSorted);
+
+  const customersSorted = sortByAscendingOrder(customers, "score");
+  console.log('customersSorted ', customersSorted);
+
+  console.log('customersSorted ', distributeCustomersToCSs(customersSorted, employeesSorted, customerSuccessAway));
+}
+
+// Inicializar uma lista vazia para cada CS, representando os clientes atribuídos a eles
+function setCustomersServedForCSs(customerSuccess) {
+  for (let index = 0; index < customerSuccess.length; index++) {
+    customerSuccess[index].attendedCustomers = [];
+  }
+  return customerSuccess;
+}
+
+// Ordenar CSs e customers em ordem crescente
+function sortByAscendingOrder(data, attribute) {
+  return [...data].sort((a, b) => a[attribute] - b[attribute]);
+}
+
+// Distribuir clientes entre os CSs
+function distributeCustomersToCSs(customers, customerSuccess, customerSuccessAway) {
+  const availableCustomerSuccess = filterAvailableCustomerSuccess(customerSuccess, customerSuccessAway);
+
+  for (const customer of customers) {
+    const csToAssign = findAvailableCSWithCapacity(availableCustomerSuccess, customer.score);
+    if (csToAssign) {
+      csToAssign.attendedCustomers.push(customer.id);
+    }
+  }
+
+  return customerSuccess;
+}
+
+function filterAvailableCustomerSuccess(customerSuccess, customerSuccessAway) {
+  return customerSuccess.filter((cs) => !customerSuccessAway.includes(cs.id));
+}
+
+function findAvailableCSWithCapacity(availableCustomerSuccess, customerScore) {
+  return availableCustomerSuccess.find((cs) => customerScore <= cs.score);
 }
 
 module.exports = customerSuccessBalancing;
-
-
